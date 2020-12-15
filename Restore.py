7d061f7c8285c7c116c7c5f96b98e8988f9c2868
Jade Rigby <jrigby@squareup.com>
2020-12-15 11:57:35 -0700

    fixes for restore and restoreBack

--- Restore.py
+++ Restore.py
@@ -6,5 +6,5 @@ import helpers
 def execute():
     branch = helpers.run_command_output('git branch --show-current', False)
     filepath = helpers.user_input('File name/path to restore from master: ')
-    helpers.run_command('git checkout master && git pull && git checkout {CURRENT_BRANCH}'.format(CURRENT_BRANCH= branch))
-    helpers.run_command('git restore --source master -- {FILE}'.format(FILE= filepath))
+    # helpers.run_command('git checkout master && git pull && git checkout {CURRENT_BRANCH}'.format(CURRENT_BRANCH= branch))
+    helpers.run_command('git restore --source origin/master -- {FILE}'.format(FILE= filepath))
--- RestoreBack.py
+++ RestoreBack.py
@@ -5,5 +5,8 @@ import helpers
 
 def execute():
     branch = helpers.run_command_output('git branch --show-current', False)
+    branch = branch.replace('\n', '')
+    branchOrigin = 'origin/{}'.format(branch)
     filepath = helpers.user_input('File name/path to restore from last commit: ')
-    helpers.run_command('git restore --source {BRANCH} -- {FILE}'.format(BRANCH= branch, FILE= filepath))
+    # helpers.run_command('git restore --source {BRANCH} -- {FILE}'.format(BRANCH= branch, FILE= filepath))
+    helpers.run_command_list(['git', 'restore', '--source', branchOrigin, '--', filepath])
