3c685f8018b6eb8c60c92679777a34ef8bac32b6
Jade Rigby <jrigby@squareup.com>
2020-12-15 09:11:11 -0700

    adding changes to help diff work properly = still not quite there; also, added to helpers file for optional profiles

--- Diff.py
+++ Diff.py
@@ -9,7 +9,10 @@ def execute():
     selection = helpers.user_selection('Select file to diff: ', listingList)
     if isinstance(selection, int):
         option = listingList[selection - 1]
-        helpers.run_command('git difftool {} {}'.format(option, settings['differ']))
+        print("option: " + option)
+        currentPath = helpers.run_command_output('pwd')
+        print('git difftool {}/{} {}'.format(currentPath, option, settings['differ']))
+        helpers.run_command('git difftool {}/{} {}'.format(currentPath, option, settings['differ']))
     elif selection is 'exit':
         print('\nExiting ...\n')
         msg.done()
