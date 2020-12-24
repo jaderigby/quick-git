cd4e2ad226b51be5a20a03e67f64df2a6866d327
Jade Rigby <jrigby@squareup.com>
2020-12-23 20:03:59 -0700

    adding selection options to status action

--- status.py
+++ status.py
@@ -5,4 +5,15 @@ import helpers
 
 def execute():
     helpers.run_command('git status')
+    selection = helpers.user_selection('Select: ', ['run "qit all"', 'stage all', 'unstage all'])
+    if selection is 1:
+        helpers.run_command('git add -A')
+        helpers.run_command('git status', False)
+        commitMessage = raw_input("Commit Message: ")
+        helpers.run_command_list(['git', 'commit', '-m', commitMessage])
+        helpers.run_command('git push')
+    elif selection is 2:
+        helpers.run_command('git add -A')
+    elif selection is 3:
+        helpers.run_command('git reset *')
     msg.done()
