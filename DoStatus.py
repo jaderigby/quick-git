f49fdd43bfac1261d5e1ed76c667852947644222
Jade Rigby <contact@jaderigby.com>
2021-01-04 00:38:37 -0700

    slight tweak to qit status, namely 'diff' instead of 'qit all'

--- DoStatus.py
+++ DoStatus.py
@@ -1,17 +1,28 @@
 import messages as msg
 import helpers
 
-# settings = helpers.get_settings()
+settings = helpers.get_settings()
 
 def execute():
     helpers.run_command('git status')
-    selection = helpers.status_selection('Selection: ', ['"qit all"', 'unstage all', 'push/exclude'])
+    selection = helpers.status_selection('Selection: ', ['diff', 'unstage all', 'push/exclude'])
     if selection == 1:
-        helpers.run_command('git add -A')
-        helpers.run_command('git status', False)
-        commitMessage = raw_input("Commit Message: ")
-        helpers.run_command_list(['git', 'commit', '-m', commitMessage])
-        helpers.run_command('git push')
+        while True:
+            listing = helpers.run_command_output('git diff --name-only')
+            listingList = listing.splitlines()
+            selection = helpers.user_selection('Select file to diff: ', listingList)
+            if isinstance(selection, int):
+                option = listingList[selection - 1]
+                currentPath = helpers.run_command_output('pwd', False)
+                currentPathList = currentPath.split('/')
+                currentPathRevised = ""
+                for item in currentPathList:
+                    if item != '':
+                        currentPathRevised += '/' + item
+                helpers.run_command('git difftool {}/{} {}'.format(currentPathRevised, option, settings['differ']))
+            elif selection == 'exit':
+                print('\nExiting ...\n')
+                break
     elif selection == 2:
         helpers.run_command('git reset *')
     elif selection == 3:
