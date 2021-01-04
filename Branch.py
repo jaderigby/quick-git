7b5c74c893711cee098e18475880cb8711e0061a
Jade Rigby <jrigby@squareup.com>
2021-01-04 09:00:26 -0700

    removing legacy color class; fixing decode bug (Python 3)

--- Branch.py
+++ Branch.py
@@ -4,17 +4,7 @@ import subprocess, helpers
 # settings = helpers.get_settings()
 
 def execute():
-    class bcolors:
-        HEADER = '\033[95m'
-        OKBLUE = '\033[94m'
-        OKCYAN = '\033[96m'
-        OKGREEN = '\033[92m'
-        WARNING = '\033[93m'
-        FAIL = '\033[91m'
-        ENDC = '\033[0m'
-        BOLD = '\033[1m'
-        UNDERLINE = '\033[4m'
-    out = subprocess.check_output(["git", "branch"])
+    out = subprocess.check_output(["git", "branch"]).decode('utf-8')
     outList = out.split()
     if '*' in outList: outList.remove('*')
     currentBranch = subprocess.check_output(["git", "branch", "--show-current"]).split()
