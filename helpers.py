6ac50ceea5698bf4d7e8ba7c109209d028cd8ce6
Jade Rigby <jrigby@squareup.com>
2021-01-13 13:11:54 -0700

    revised param referenced for local remove function to correct param

--- helpers.py
+++ helpers.py
@@ -201,7 +201,7 @@ def user_list_selection(DESCRIPTION, LIST):
 def remove_local_branch(CURRENT_BRANCH, BRANCH_TO_REMOVE):
 	if BRANCH_TO_REMOVE == CURRENT_BRANCH:
 		run_command('git checkout master')
-	run_command('git branch -D {}'.format(CURRENT_BRANCH))
+	run_command('git branch -D {}'.format(BRANCH_TO_REMOVE))
 
 def remove_remote_branch(BRANCH_TO_REMOVE):
 	run_command('git push origin --delete {}'.format(BRANCH_TO_REMOVE))
