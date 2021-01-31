e15d45024a7b655d1df804cb59a4a03347a3da58
Jade Rigby <contact@jaderigby.com>
2021-01-30 20:23:08 -0700

    'qit status' cleanup

--- DoStatus.py
+++ DoStatus.py
@@ -16,11 +16,9 @@ def execute():
             if settings:
                 if 'differ' in settings:
                     while True:
-                        listing = helpers.run_command_output('git diff --name-only')
-                        listingList = listing.splitlines()
-                        selection = helpers.user_selection('Select file to diff: ', listingList)
+                        selection = helpers.user_selection('Select file to diff: ', fileList)
                         if isinstance(selection, int):
-                            option = listingList[selection - 1]
+                            option = fileList[selection - 1]
                             currentPath = helpers.run_command_output('pwd', False)
                             helpers.run_command('git difftool {}/{} {}'.format(currentPath.replace('\n', ''), option, settings['differ']))
                         elif selection == 'exit':
